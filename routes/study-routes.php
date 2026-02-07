e9fcc4805af2cb48b4ff31f47652dff5c96f76ca
Johannes Brock <info@johannes-brock.de>
2026-02-07 23:48:52 +0100

    fix routes

--- routes/study-routes.php
+++ routes/study-routes.php
@@ -5,11 +5,11 @@ use Nywerk\Study\Http\Controllers\FlashcardPrintController;
 
 Route::group(['middleware' => ['web', 'auth', 'verified']], function (): void {
     Route::livewire('study/study-materials', 'study-materials-list')->name('study.study-materials');
-    Route::livewire('study/study-material/{model}', 'study-material-detail')->name('study.study-material.detail');
+    Route::livewire('study/study-material/{modelId}', 'study-material-detail')->name('study.study-material.detail');
     Route::livewire('study/summaries', 'summaries-list')->name('study.summaries');
-    Route::livewire('study/summary/{model}', 'summary-detail')->name('study.summary.detail');
+    Route::livewire('study/summary/{modelId}', 'summary-detail')->name('study.summary.detail');
     Route::livewire('study/flashcards', 'flashcards-list')->name('study.flashcards');
-    Route::livewire('study/flashcard/{model}', 'flashcard-detail')->name('study.flashcard.detail');
+    Route::livewire('study/flashcard/{modelId}', 'flashcard-detail')->name('study.flashcard.detail');
     Route::livewire('study/flashcards-print', 'flashcard-print-detail')->name('study.flashcards-print');
     Route::get('study/flashcards-print/pdf', [FlashcardPrintController::class, 'print'])->name('study.flashcards-print.pdf');
 });
